d00da9bad1aefbc9cef7cc3befa2e9da2a179016
SHARJEEL-DS <sharjeelsohail697@gmai.com>
2023-05-16 08:41:54 +0500

    project setup

--- components/ImageSlider.js
+++ components/ImageSlider.js
@@ -1,28 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import styles from '../styles/Home.module.css'
-const ImageSlider = ({ images }) => {
-  const [currentIndex, setCurrentIndex] = useState(0);
+import React from 'react'
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
-    }, 3000);
-
-    return () => clearInterval(interval);
-  }, [images]);
-
+function ImageSlider() {
   return (
-    <div className="image-slider">
-      {images.map((image, index) => (
-        <img
-          key={index}
-          src={image}
-          alt={`Image ${index}`}
-          className={index === currentIndex ? 'active' : ''}
-        />
-      ))}
+    <div>
+      
     </div>
-  );
-};
+  )
+}
 
-export default ImageSlider;
+export default ImageSlider
